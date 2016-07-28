82f179c30513089027fe9246fc14b8fb3c38414d
Ivan Baena <ivan.baena04@gmail.com>
2016-07-28 09:50:24 -0400

    added nav bar; added anchor scroll function

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,44 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-  let showNav = false;
-
-
-  $('#menu').on('click', function(){
-    showNav = !showNav;
-    if(showNav === true){
-      $('#tagNav').removeClass('hidden');
-      $('#tagNav').addClass('slideInRight');
-    }else{
-      $('#tagNav').removeClass('slideInRight');
-      $('#tagNav').addClass('slideOutRight');
-      $('#tagNav').addClass('hidden');
-    }
-
-  });
+    let showNav = false;
 
 
+    $('#menu-burger').on('click', function() {
+        showNav = !showNav;
+        console.log(showNav);
+        if (showNav === true) {
+            $('#tagNav').removeClass('hidden');
+            $('#tagNav').removeClass('animated slideOutLeft');
+            $('#tagNav').addClass('animated slideInLeft');
+        } else {
+            $('#tagNav').removeClass('animated slideInRight');
+            $('#tagNav').addClass('animated slideOutLeft');
+            $('#tagNav').addClass('hidden');
+        }
+
+    });
+
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
 
 
 
